526ac8d5daa28ed4e5a7990047ec5154a91333c1
mule-heo <devheojinwoo@gmail.com>
2024-04-14 03:10:30 +0900

    add card without icon

--- flutter/flutter_tutorial/lib/main.dart
+++ flutter/flutter_tutorial/lib/main.dart
@@ -16,72 +16,137 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // 나중에 code action으로 Padding 지우기
-      home: Padding(
-        padding: const EdgeInsets.all(100),
-        child: Scaffold(
-            backgroundColor: const Color(0xFF181818),
-            body: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                    Column(
-                      children: [
-                        Text('Hey, Mule',
-                            style: TextStyle(
-                                color: Colors.white.withOpacity(0.7),
-                                fontSize: 36,
-                                fontWeight: FontWeight.w800)),
-                        Text('Welcome back',
-                            style: TextStyle(
+      home: Scaffold(
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  Column(
+                    children: [
+                      Text('Hey, Mule',
+                          style: TextStyle(
                               color: Colors.white.withOpacity(0.7),
-                              fontSize: 16,
-                            )),
+                              fontSize: 36,
+                              fontWeight: FontWeight.w800)),
+                      Text('Welcome back',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.7),
+                            fontSize: 16,
+                          )),
+                    ],
+                  ),
+                ]),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text('Total Balance',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white.withOpacity(0.8),
+                    )),
+                const Text('\$1 000 000',
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(
+                        text: "Transfer",
+                        bgColor: Colors.amber,
+                        textColor: Colors.black),
+                    Button(
+                      text: "Request",
+                      bgColor: Colors.black,
+                      textColor: Colors.white,
+                    )
+                  ],
+                ),
+                const SizedBox(height: 100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text('Wallets',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600,
+                        )),
+                    Text('View All',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF212223),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Euro',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 32,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(children: [
+                              const Text(
+                                '6,428',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.8),
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ])
+                          ],
+                        ),
                       ],
                     ),
-                  ]),
-                  const SizedBox(
-                    height: 5,
                   ),
-                  Text('Total Balance',
-                      style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white.withOpacity(0.8),
-                      )),
-                  const Text('\$1 000 000',
-                      style: TextStyle(
-                        fontSize: 42,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.white,
-                      )),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Button(
-                          text: "Transfer",
-                          bgColor: Colors.amber,
-                          textColor: Colors.black),
-                      Button(
-                        text: "Request",
-                        bgColor: Colors.black,
-                        textColor: Colors.white,
-                      )
-                    ],
-                  )
-                ],
-              ),
-            )),
-      ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
